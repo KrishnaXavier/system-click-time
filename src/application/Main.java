5937b307007f101c623dd91fb3b94a605170cc1c
KrishnaXavier <tsixav@gmail.com>
2017-07-23 21:30:17 -0300

    Configuration window

---
+++ src/application/Main.java
@@ -0,0 +1,17 @@
+package application;
+
+/**
+ *
+ * @author Krishna Xavier
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConfigurationWindow config = new ConfigurationWindow();
+        config.setVisible(true);
+    }
+    
+}
